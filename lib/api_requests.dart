691394d0da353b6de1f0c393cce95018949077cd
Ritwik Takkar <44250480+tryRestartingIt@users.noreply.github.com>
2020-07-18 18:16:51 +0800

    remove unnecessary shit

--- lib/api_requests.dart
+++ lib/api_requests.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shorten_my_URL/url_model.dart';
-import 'package:shorten_my_URL/dialogs.dart';
 import 'dart:convert';
 
 var postURL = 'https://cleanuri.com/api/v1/shorten';
@@ -13,7 +12,6 @@ Future<ShortenedURL> getShortenedURL(String longURL) async {
     // If the server did return a 200 OK response,
     // then parse the JSON.
     debugPrint(response.body.toString());
-    //debugPrint(response.body.toString());
     return ShortenedURL.fromJson(json.decode(response.body));
   } else {
     // If the server did not return a 200 OK response,
