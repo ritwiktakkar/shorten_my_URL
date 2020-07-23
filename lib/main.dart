5e3a91718e021faac81562621985525157649b33
Ritwik Takkar <44250480+tryRestartingIt@users.noreply.github.com>
2020-07-24 00:02:29 +0800

    merge v1_fix_snackbar branch

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,9 @@ class MyApp extends StatelessWidget {
         statusBarBrightness: Brightness.dark,
       ),
     );
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
     return new MaterialApp(
       debugShowCheckedModeBanner: false, // hide debug banner from top left
       home: new HomePage(),
@@ -112,7 +115,6 @@ class _HomePageState extends State<HomePage> {
                   top: 20,
                 ),
                 child: TextField(
-                  // TODO: add padding to text inside
                   autocorrect: false, // URL so no need
                   onTap: () {
                     FocusScope.of(context).unfocus();
@@ -120,18 +122,20 @@ class _HomePageState extends State<HomePage> {
                   controller: inputController,
                   keyboardType: TextInputType.url,
                   decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: const BorderRadius.all(
-                          const Radius.circular(40.0),
-                        ),
+                    border: OutlineInputBorder(
+                      borderRadius: const BorderRadius.all(
+                        const Radius.circular(40.0),
                       ),
-                      filled: true,
-                      hintStyle: TextStyle(
-                          color: Colors.grey[600],
-                          fontSize: 16,
-                          fontWeight: FontWeight.w300),
-                      hintText: "Enter the URL to shorten here",
-                      fillColor: Colors.white12),
+                    ),
+                    contentPadding: EdgeInsets.fromLTRB(25, 8, 0, 8),
+                    filled: true,
+                    hintStyle: TextStyle(
+                        color: Colors.grey[600],
+                        fontSize: 16,
+                        fontWeight: FontWeight.w300),
+                    hintText: "Enter the URL to shorten here",
+                    fillColor: Colors.white12,
+                  ),
                   style: TextStyle(
                     color: Colors.white,
                     fontSize: 18,
@@ -275,35 +279,38 @@ class _HomePageState extends State<HomePage> {
                                 // TODO: add some way for user to know that their request is being executed and they need to wait
                                 longURL = inputController.text;
                                 shortURL = await API.getShortenedURL(longURL);
-                                if (shortURL == null) {
+                                if (shortURL == null ||
+                                    shortURL.shortenedURL == '') {
                                   Dialogs.showShorteningURLError(context);
-                                }
-                                HapticFeedback.lightImpact();
-                                final snackBar = SnackBar(
-                                  behavior: SnackBarBehavior.floating,
-                                  backgroundColor: Colors.orange[300],
-                                  content: Row(
-                                    children: <Widget>[
-                                      Icon(
-                                        Icons.check,
-                                      ),
-                                      SizedBox(
-                                        width: 10,
-                                      ),
-                                      AutoSizeText(
-                                        'URL successfully shortened',
-                                        textAlign: TextAlign.center,
-                                        style: TextStyle(
-                                          fontSize: 15,
+                                } else if (shortURL != null ||
+                                    shortURL.shortenedURL != '') {
+                                  HapticFeedback.lightImpact();
+                                  final snackBar = SnackBar(
+                                    behavior: SnackBarBehavior.floating,
+                                    backgroundColor: Colors.orange[300],
+                                    content: Row(
+                                      children: <Widget>[
+                                        Icon(
+                                          Icons.check,
                                         ),
-                                        maxLines: 1,
-                                      ),
-                                    ],
-                                  ),
-                                );
-                                Scaffold.of(context).hideCurrentSnackBar();
-                                Scaffold.of(context).showSnackBar(snackBar);
-                                outputController.text = shortURL.shortenedURL;
+                                        SizedBox(
+                                          width: 10,
+                                        ),
+                                        AutoSizeText(
+                                          'URL successfully shortened',
+                                          textAlign: TextAlign.center,
+                                          style: TextStyle(
+                                            fontSize: 15,
+                                          ),
+                                          maxLines: 1,
+                                        ),
+                                      ],
+                                    ),
+                                  );
+                                  Scaffold.of(context).hideCurrentSnackBar();
+                                  Scaffold.of(context).showSnackBar(snackBar);
+                                  outputController.text = shortURL.shortenedURL;
+                                }
                               }
                             } else {
                               Dialogs.showInvalidInput(context);
@@ -390,22 +397,23 @@ class _HomePageState extends State<HomePage> {
                   top: 20,
                 ),
                 child: TextField(
-                  // TODO: add padding to text inside
                   readOnly: true,
                   controller: outputController,
                   decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: const BorderRadius.all(
-                          const Radius.circular(40.0),
-                        ),
+                    border: OutlineInputBorder(
+                      borderRadius: const BorderRadius.all(
+                        const Radius.circular(40.0),
                       ),
-                      filled: true,
-                      hintStyle: TextStyle(
-                          color: Colors.grey[600],
-                          fontSize: 16,
-                          fontWeight: FontWeight.w300),
-                      hintText: "The shortened URL will appear here",
-                      fillColor: Colors.white12),
+                    ),
+                    contentPadding: EdgeInsets.fromLTRB(25, 8, 0, 8),
+                    filled: true,
+                    hintStyle: TextStyle(
+                        color: Colors.grey[600],
+                        fontSize: 16,
+                        fontWeight: FontWeight.w300),
+                    hintText: "The shortened URL will appear here",
+                    fillColor: Colors.white12,
+                  ),
                   style: TextStyle(
                     color: Colors.white,
                     fontSize: 18,
